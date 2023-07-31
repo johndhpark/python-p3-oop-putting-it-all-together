f914c609f2620026f207025ed37e9749325b1ec5
John DH. Park <john.dh.park@icloud.com>
2023-07-31 05:16:40 -0700

    Completed Object-Oriented Programming Lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size = 10):
+        self.brand = brand
+        self.size = size
+
+    def set_size(self, size):
+        if type(size) != int:
+            print("size must be an integer")
+            return
+        self._size = size
+
+    def get_size(self):
+        return self._size
+
+
+    size = property(get_size, set_size)
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
